4ccc31f781da2ce1ea7d8b9263cca55569bd189e
Christian <christianparizeau@gmail.com>
2020-04-05 16:58:04 -0700

    removed text divs from the page

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -22,7 +22,6 @@ export default class App extends React.Component {
   render() {
     return (
       <>
-        <h1>Hello World</h1>
         <div className="class-card-container">
           {this.state.classCards.map((element, index) => {
             return (
